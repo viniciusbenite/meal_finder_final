0faa91c6b6e9d7c0d7dd58f37b23df74c7ebb410
José Frias <josefrias99@ua.pt>
2020-03-12 19:45:09 +0000

    changes in first time users activity

--- app/src/main/java/com/example/mealfinder/FirstTimeUsers.java
+++ app/src/main/java/com/example/mealfinder/FirstTimeUsers.java
@@ -1,13 +1,16 @@
 package com.example.mealfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.GridLayout;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -15,58 +18,34 @@ import java.util.ArrayList;
 public class FirstTimeUsers extends AppCompatActivity {
     Button button;
     ArrayList<String> diets_saved=new ArrayList<>();
+    GridLayout gridLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_time_users);
         button=findViewById(R.id.button2);
+        gridLayout=findViewById(R.id.gridLayout);
+
+        setToogleEvent(gridLayout);
 
     }
 
-    public void onCheckboxClicked(View view){
-        boolean checked = ((CheckBox) view).isChecked();
-        switch(view.getId()) {
-
-            case R.id.checkBox:
-                 if (checked){
-                     Toast.makeText(getApplicationContext(), "Vegan added", Toast.LENGTH_LONG);
-                     diets_saved.add("Vegan");
-
-                 }
-                 else{
-                     Toast.makeText(getApplicationContext(), "Vegan removed", Toast.LENGTH_LONG);
-                     diets_saved.remove("Vegan");
-                 }
-
-                 break;
-        case R.id.checkBox2:
-                if (checked){
-                    Toast.makeText(getApplicationContext(), "Vegetarian added", Toast.LENGTH_LONG);
-                    diets_saved.add("Vegetarian");
+    private void setToogleEvent( GridLayout gridLayout){
+        for (int i=0; i<gridLayout.getChildCount();i++){
+            final CardView cardView=(CardView) gridLayout.getChildAt(i);
+            final int finalI=i;
+            cardView.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                   if (cardView.getCardBackgroundColor().getDefaultColor()==-1){
+                       cardView.setCardBackgroundColor(Color.parseColor("#0F9D58"));
+                   }
+                   else{
+                       cardView.setCardBackgroundColor(Color.parseColor("#FFFFFF"));
+                   }
                 }
-
-                else{
-                    Toast.makeText(getApplicationContext(), "Vegetarian removed", Toast.LENGTH_LONG);
-                    diets_saved.remove("Vegetarian");
-                }
-
-                break;
-
-            case R.id.checkBox3:
-
-                if (checked){
-                    Toast.makeText(getApplicationContext(), "Macrobiotic added", Toast.LENGTH_LONG);
-                    diets_saved.add("Macrobiotic");
-                }
-
-                else{
-                    Toast.makeText(getApplicationContext(), "Macrobitotic removed", Toast.LENGTH_LONG);
-                    diets_saved.remove("Macrobiotic");
-                }
-
-                break;
-
+            });
         }
     }
     public void go_to_main(View view){
