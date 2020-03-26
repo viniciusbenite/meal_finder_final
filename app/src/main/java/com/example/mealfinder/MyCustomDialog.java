8435458b5a49173750b7625ed92e226c3b64531c
José Frias <josefrias99@ua.pt>
2020-03-26 14:32:24 +0000

    beggining of upload picture and date picker in new log

--- app/src/main/java/com/example/mealfinder/MyCustomDialog.java
+++ app/src/main/java/com/example/mealfinder/MyCustomDialog.java
@@ -1,18 +1,38 @@
 package com.example.mealfinder;
+import android.app.DatePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
+
+import static android.app.Activity.RESULT_OK;
+
+
 public class MyCustomDialog extends DialogFragment {
     private static final String TAG="MyCustomDialog";
 
@@ -22,10 +42,11 @@ public class MyCustomDialog extends DialogFragment {
     public OnInputSelected onInputSelected;
     TextView log_name;
     TextView meal_name;
-    TextView startDate;
+    TextView date_log;
     Button submit;
     Button cancel;
-
+    ImageView chooseImage;
+    Calendar myCalendar;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -33,10 +54,40 @@ public class MyCustomDialog extends DialogFragment {
         View view = inflater.inflate(R.layout.add_log_dialog, container, false);
         log_name=view.findViewById(R.id.choose_log_name);
         meal_name=view.findViewById(R.id.choose_meal_name);
-        startDate=view.findViewById(R.id.choose_startDate);
+        date_log=view.findViewById(R.id.choose_startDate);
         submit=view.findViewById(R.id.buttonSubmit);
         cancel=view.findViewById(R.id.buttonCancel);
+        chooseImage=view.findViewById(R.id.choose_photo);
+        myCalendar = Calendar.getInstance();
 
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+        };
+
+        date_log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(getContext(), date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+        chooseImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
 
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,11 +99,24 @@ public class MyCustomDialog extends DialogFragment {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String input=log_name.getText().toString();
-                if (!input.equals("")){
-                    onInputSelected.sendInput(input);
-
+                String log=log_name.getText().toString();
+                String picture=chooseImage.toString();
+                String meal=meal_name.getText().toString();
+                String date_str=date_log.getText().toString();
+                String toSend="";
+                if (!log.equals("")){
+                    toSend+=log;
                 }
+                if (!picture.equals("")){
+                    toSend+=", "+picture;
+                }
+                if (!meal.equals("")){
+                    toSend+=", "+meal;
+                }
+                if (!date_str.equals("")){
+                    toSend+=", "+date;
+                }
+                onInputSelected.sendInput(toSend);
                 getDialog().dismiss();
             }
         });
@@ -67,6 +131,74 @@ public class MyCustomDialog extends DialogFragment {
         }catch (ClassCastException e){
             Log.e(TAG, "onAttach: ClassCastException: "+e.getMessage());
         }
+    }
+
+    private void selectImage(){
+        final CharSequence[] options = { "Take Photo", "Choose from Gallery","Cancel" };
+        AlertDialog.Builder builder= new AlertDialog.Builder(getContext());
+        builder.setTitle("Upload Photo");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (options[item].equals("Take Photo"))
+                {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    File f = new File(android.os.Environment.getExternalStorageDirectory(), "temp.jpg");
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                    startActivityForResult(intent, 1);
+                }
+                else if (options[item].equals("Choose from Gallery"))
+                {
+                    Intent intent = new   Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intent, 2);
+                }
+                else if (options[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap;
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+                File f = new File(Environment.getExternalStorageDirectory().toString());
+                for (File temp : f.listFiles()) {
+                    if (temp.getName().equals("temp.jpg")) {
+                        f = temp;
+                        break;
+                    }
+                }
+                try {
+                    BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+                    bitmap = BitmapFactory.decodeFile(f.getAbsolutePath(), bitmapOptions);
+                    chooseImage.setImageBitmap(bitmap);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            else if (requestCode == 2) {
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                Cursor cursor = getContext().getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                cursor.moveToFirst();
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                String picturePath = cursor.getString(columnIndex);
+                cursor.close();
+                bitmap = BitmapFactory.decodeFile(picturePath);
+                chooseImage.setImageBitmap(bitmap);
+            }
+        }
+    }
+    private void updateLabel() {
+        String myFormat = "MM/dd/yyyy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.UK);
+        date_log.setText(sdf.format(myCalendar.getTime()));
     }
 
 
